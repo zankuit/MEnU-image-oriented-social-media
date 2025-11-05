de9162944ee9d5da609117320740949755b04556
zankuit <24520265@gm.uit.edu.vn>
2025-11-05 17:38:16 +0700

    Chuyển từ Framework cũ sang .NET 8

--- MEnU/Forms/Login.cs
+++ MEnU/Forms/Login.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace MEnU
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
