4b9d9b187d902a67d0a6182eebe755d4aee02fa5
HanhNguyen <24520452@gm.uit.edu.vn>
2025-11-09 12:38:56 +0700

    Them code thiet ke form

---
+++ DemoDeAn/Home.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace DemoDeAn
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            closebtn = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(41, 128, 180);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 530);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(41, 128, 185);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.Control;
+            button2.Image = Properties.Resources.R;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(131, 253);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 47);
+            button2.TabIndex = 11;
+            button2.Text = "Setting";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(41, 128, 185);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.Control;
+            button1.Image = Properties.Resources.home_150499_1280;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(131, 200);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 47);
+            button1.TabIndex = 10;
+            button1.Text = "Home";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Transparent;
+            label3.Location = new Point(93, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 27);
+            label3.TabIndex = 9;
+            label3.Text = "Username";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(41, 128, 185);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Century Gothic", 9F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.Control;
+            button3.Location = new Point(95, 459);
+            button3.Name = "button3";
+            button3.Size = new Size(123, 47);
+            button3.TabIndex = 8;
+            button3.Text = "Đăng xuất";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.User;
+            pictureBox1.Location = new Point(110, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(80, 77);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(closebtn);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(300, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(450, 530);
+            panel2.TabIndex = 1;
+            // 
+            // closebtn
+            // 
+            closebtn.Cursor = Cursors.Hand;
+            closebtn.FlatAppearance.BorderSize = 0;
+            closebtn.FlatStyle = FlatStyle.Flat;
+            closebtn.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            closebtn.ForeColor = Color.FromArgb(41, 128, 185);
+            closebtn.Location = new Point(410, 0);
+            closebtn.Name = "closebtn";
+            closebtn.Size = new Size(40, 40);
+            closebtn.TabIndex = 0;
+            closebtn.Text = "X";
+            closebtn.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(750, 530);
+            ControlBox = false;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Home";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Button closebtn;
+        private ContextMenuStrip contextMenuStrip1;
+        private Button button3;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+    }
+}
